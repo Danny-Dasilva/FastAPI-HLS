3021e4aba406bf38914f6a935458b26a2d2d3853
Danny-Dasilva <yahchayildasilva@gmail.com>
2022-04-16 11:14:28 -0400

    update main file
    Former-commit-id: ad50b1a3a10847aed18000f1e5bdf67d6c29fcbc

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 import json
 from fastapi import FastAPI, Response
 from fastapi.staticfiles import StaticFiles
-from starlette.responses import FileResponse
+from starlette.responses import FileResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
@@ -18,13 +18,22 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+def sanitize(item: str) -> str:
+    """Make sure they can't do a directory transversal attack"""
+    return item.replace("\\", "").replace("/", "")
 
 
 @app.get("/video/{fileName}")
 async def video(response: Response, fileName: str):
     response.headers["Content-Type"] = "application/x-mpegURL"
-    return FileResponse("video/" + fileName, filename=fileName)
+    return FileResponse("video/" + sanitize(fileName), filename=fileName)
 
+@app.get("/video/{video_name}/{segment_number}.ts", response_class=FileResponse)
+async def get_segment(video_name: str, segment_number: str):
+    segment = "video" / sanitize(video_name) / f"{sanitize(segment_number)}.ts"
+    if not segment.exists():
+        return HTMLResponse(status_code=404)
+    return segment
 
 # @app.get("/markers")
 # async def markers(response: Response, ts_start: float = -1.0):
---
+++ video/convert.sh
@@ -0,0 +1,10 @@
+ffmpeg -i my_video.mp4                    \
+       -c:v libx264                       \
+       -b:v 3000k                         \
+       -bufsize:v 6000k                   \
+       -maxrate:v 6000k                   \
+       -c:a aac                           \
+       -b:a 128k                          \
+       -hls_list_size 0                   \
+       -hls_time 6                        \
+       video.m3u8
