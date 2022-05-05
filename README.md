9c1b2a641eb517363f2aa3c19c3a8d9850d53582
Danny-Dasilva <yahchayildasilva@gmail.com>
2022-05-05 16:10:04 -0400

    functional adaptive_bitrate_streaming write
    Former-commit-id: 9543345046de20278ae6ff56549ea1d2d5813c62

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # FastAPI-HLS
 FastAPI HLS streaming test repo
+
+
+
+## M3U8 master playlist creator ##
+* Uses FFMPEG tool in Python by using a OS command call
+* Install FFMPEG for Ubuntu OS using terminal. Follow instructions here: https://tecadmin.net/install-ffmpeg-on-linux/
+* Following are explanations of the options used in the FFMPEG tool:
+	- `-c:v libx264` is the codec:video. Here we use the x264 codec to have a h264 output format
+	- `-crf 20` is the video quality. 51 is the worst quality and 1 the best
+	- `-g 5 -keyint_min 5` sets group picture size to 5. Video source is 5 FPS. So each second will start with a new picture group. Create key frame every 5 frames and start new picture group
+	- `-sc_threshold 0` indicates don't create key frames on scene change, only according to -g
+	- `-hls_time 4` slices the video and audio into segments with a duration of 4 seconds
+	- `-hls_flags independent_segments` indicates that all media samples in a Media Segment can be decoded without information from other segments
+	- `-b:v:0` sets the bitate for stream 0 (the lower quality)
+	- `-filter:v:0 scale=640:360` scales stream 0 to the desired resolution
+	- `-map v:x` tells ffmpeg that a new video variant starts. x is the number of input video signal. Since only one video input, index 0 is used here
+	- `-f hls` defines the output format HLS
+	- `-var_stream_map “v:0 v:1 v:2 v:3”` tells FFmpeg what streams are combined together. A space separates each variant and everything that should be placed together is concatenated with a comma(if audio as well)
+	- `-master_pl_name master.m3u8` creates the new master playlist that contains the list of streams
+	- `-hls_segment_filename stream_%v/data%03d.ts` sets the name of the segment file. Folder is added named “stream_%v”. %v is the stream variant. The %03d in the filename is a formatted number. 03 means fill the number up with 0 up to 3 digits
+	- `-use_localtime_mkdir 1` adds base-url to the playlist files so that the variant streams' segments can be located
+* Refer to FFMPEG Documentation for further use or check out this tutorial: https://www.martin-riedl.de/2018/08/24/using-ffmpeg-as-a-hls-streaming-server-part-1/
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from fastapi import (
     Query,
     HTTPException,
 )
+import subprocess
 from fastapi import Path as PathParam
 from pathlib import Path
 from typing import Optional
@@ -27,37 +28,20 @@ from pathlib import Path
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse
 
-  
 
 templates = Jinja2Templates(directory="static")
 app = FastAPI()
 base_path = "./videos/"
 
 
-# class SanatizedPathParam(str):
-#     """
-#     TODO Document
-#     """
-
-#     def __new__(
-#         cls,
-#         path: str = Query(
-#             "test",
-#             title="The description of the item",
-#         ),
-#     ):
-#         return path.replace("\\", "").replace("/", "")
-
-
-class UploadParam(str):
+class FolderParam(str):
     """
     TODO Document
     """
 
     def __new__(
         cls,
-        path: Optional[str] =
-        Query(
+        path: Optional[str] = Query(
             "",
             title="The description of the item",
         ),
@@ -65,20 +49,18 @@ class UploadParam(str):
         if path in next(os.walk(base_path))[1]:
             return path
         else:
-            return ""      
+            return ""
 
 
 class SanatizedPathParam(BaseModel):
-    """
-    TODO Document
-    """
+    """"""
 
     path: str
 
     @classmethod
     def inject(
         cls,
-        path: str = PathParam(
+        path: str = Query(
             "",
             description=("Text to explain this value"),
         ),
@@ -98,7 +80,7 @@ async def create_directory(
 @app.get("/")
 async def get_videos():
     data = {}
-    files =  list(Path("./videos/").rglob("*.m3u8"))
+    files = list(Path("./videos/").rglob("*.m3u8"))
     for file in files:
         filepath = os.path.relpath(file, base_path)
         data[file.stem] = f"http://localhost:8081/watch/{filepath}"
@@ -106,22 +88,23 @@ async def get_videos():
 
 
 @app.get("/video/{directory_name}/{path:path}")
-async def stream_video(response: Response, directory_name,path):
+async def stream_video(response: Response, directory_name, path):
     response.headers["Content-Type"] = "application/x-mpegURL"
     path = f"{directory_name}/{path}"
     return FileResponse(f"./videos/{path}", filename=path)
 
 
 @app.get("/watch/{directory_name}/{file_name}")
-async def watch_video(request: Request,directory_name, file_name):
+async def watch_video(request: Request, directory_name, file_name):
     return templates.TemplateResponse(
-        f"index.html", {"request": request, "folder_name": directory_name, "video_name": file_name}
+        f"index.html",
+        {"request": request, "folder_name": directory_name, "video_name": file_name},
     )
 
 
 def ffmpeg_conversion(path, file: UploadFile):
     ffmpeg.input(path).output(
-       f"{Path(path).with_suffix('')}.m3u8",
+        f"{Path(path).with_suffix('')}.m3u8",
         vcodec="libx264",
         acodec="aac",
         bitrate="3000k",
@@ -135,22 +118,61 @@ def ffmpeg_conversion(path, file: UploadFile):
     ).run()
 
 
+def ffmpeg_conversion(path):
+    ffmpeg.input(path).output(
+        f"{Path(path).with_suffix('')}.m3u8",
+        vcodec="libx264",
+        acodec="aac",
+        bitrate="3000k",
+        vbufsize="6000k",
+        vmaxrate="6000k",
+        format="hls",
+        start_number=0,
+        hls_time=10,
+        hls_list_size=0,
+        audio_bitrate="128k",
+    ).run()
 
 
+def adaptive_bitrate_ffmpeg(path):
+    command = f"""ffmpeg -i {path} -c:v libx264 -crf 20 -g 5 -keyint_min 5 -sc_threshold 0 -hls_time 4 -hls_flags independent_segments \
+  -b:v:0 800k -filter:v:0 scale=640:360 \
+  -b:v:1 1200k -filter:v:1 scale=842:480 \
+  -b:v:2 2400k -filter:v:2 scale=1280:720 \
+  -b:v:3 4800k -filter:v:3 scale=1920:1080 \
+  -map 0:v -map 0:v -map 0:v -map 0:v -f hls -var_stream_map 'v:0 v:1 v:2 v:3' \
+  -master_pl_name {Path(path).stem}.m3u8 \
+  -hls_segment_filename {Path(path).with_suffix('')}_%v/data%03d.ts \
+  -use_localtime_mkdir 1 \
+  {Path(path).with_suffix('')}_%v.m3u8"""
+    subprocess.call(command, shell=True)
 
 
-
-@app.post("/uploadfile/")
+@app.post("/upload/")
 async def create_upload_file(
     file: UploadFile,
     background_tasks: BackgroundTasks,
-    folder: UploadParam = Depends(),
+    folder: FolderParam = Depends(),
 ):
     path = f"./videos/{folder}/{file.filename}"
     async with aiofiles.open(path, "wb") as out_file:
         content = await file.read()  # async read
         await out_file.write(content)
-    background_tasks.add_task(ffmpeg_conversion, path, file)
+    background_tasks.add_task(ffmpeg_conversion, path)
+    return {"filename": file.filename, "fileb_content_type": file.content_type}
+
+
+@app.post("/upload_adaptive_bitrate/")
+async def upload_adaptive_bitrate(
+    file: UploadFile,
+    background_tasks: BackgroundTasks,
+    folder: FolderParam = Depends(),
+):
+    path = f"./videos/{folder}/{file.filename}"
+    async with aiofiles.open(path, "wb") as out_file:
+        content = await file.read()  # async read
+        await out_file.write(content)
+    background_tasks.add_task(adaptive_bitrate_ffmpeg, path)
     return {"filename": file.filename, "fileb_content_type": file.content_type}
 
 
